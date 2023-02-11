cb27251d5e3c9b2ac2ed2ceb652d115d579dd6b2
HackieMesser <41595086+HackieMesser@users.noreply.github.com>
2023-02-11 12:00:12 +0100

    Update bigquery.sql

--- week3/bigquery.sql
+++ week3/bigquery.sql
@@ -3,3 +3,9 @@ SELECT station_id,name FROM `bigquery-public-data.new_york_citibike.citibike_sta
 
 
 /*stuck here need to upload these files and then align the csv slugs to my slugs*/
+/*need to align table creation to my file structure too*/
+CREATE OR REPLACE EXTERNAL TABLE `taxi-rides-ny.nytaxi.external_yellow_tripdata`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://nyc-tl-data/trip data/yellow_tripdata_2019-*.csv', 'gs://nyc-tl-data/trip data/yellow_tripdata_2020-*.csv']
+);
